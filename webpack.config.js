c7642248a2f376a4be4fcf7336637dedfa4c8e10
Jason Madsen <jmadsen@instructure.com>
2015-03-26 11:38:24 -0600

    [FIXED] specificity in process.env

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {'NODE_ENV': JSON.stringify(process.env.NODE_ENV)}
+      'process': {'env': JSON.stringify(process.env)}
     })
   ]
 };
